4941e1d5470fa8c4b24a4999750c30d06c6bf4c0
Daniel Hanák <daniel.hanak@outlook.cz>
2023-04-10 15:23:34 +0200

    added connection string and code first migration

--- EntityFramework_DotNet7_SQLServer/Program.cs
+++ EntityFramework_DotNet7_SQLServer/Program.cs
@@ -3,14 +3,15 @@ global using EntityFramework_DotNet7_SQLServer.Services.CharacterService;
 global using EntityFramework_DotNet7_SQLServer.Dtos.Character;
 global using AutoMapper;
 global using Microsoft.EntityFrameworkCore;
+global using EntityFramework_DotNet7_SQLServer.Data;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);

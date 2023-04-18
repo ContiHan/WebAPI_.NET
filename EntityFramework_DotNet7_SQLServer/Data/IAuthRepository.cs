81992ec39e41e103f162e457fa89c8cb44b804c0
Daniel Hanák <daniel.hanak@outlook.cz>
2023-04-18 18:54:20 +0200

    added auth data

---
+++ EntityFramework_DotNet7_SQLServer/Data/IAuthRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace EntityFramework_DotNet7_SQLServer.Data;
+
+public interface IAuthRepository
+{
+    public Task<ServiceResponse<int>> Register(User user, string password);
+    public Task<ServiceResponse<string>> Login(string username, string password);
+    public Task<bool> UserExists(string username);
+}

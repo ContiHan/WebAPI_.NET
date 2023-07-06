6a63d6a497de6ccc7c021dccfa206b3b58645f5e
Daniel Hanák <daniel.hanak@outlook.cz>
2023-07-06 20:55:34 +0200

    added authentication

--- EntityFramework_DotNet7_SQLServer/Controllers/CharacterController.cs
+++ EntityFramework_DotNet7_SQLServer/Controllers/CharacterController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EntityFramework_DotNet7_SQLServer.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class CharacterController : ControllerBase
--- EntityFramework_DotNet7_SQLServer/Program.cs
+++ EntityFramework_DotNet7_SQLServer/Program.cs
@@ -5,6 +5,9 @@ global using EntityFramework_DotNet7_SQLServer.Dtos.User;
 global using AutoMapper;
 global using Microsoft.EntityFrameworkCore;
 global using EntityFramework_DotNet7_SQLServer.Data;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +21,19 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<ICharacterService, CharacterServiceSqlDb>();
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey =
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value!)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 var app = builder.Build();
 
@@ -29,6 +45,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 

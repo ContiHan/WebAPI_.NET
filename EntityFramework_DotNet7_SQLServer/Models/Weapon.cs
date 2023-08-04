376c643e223deb479d1b1d4aa7cca53127df27af
Daniel Hanák <daniel.hanak@outlook.cz>
2023-08-04 22:28:17 +0200

    added weapon as one to one relationship

---
+++ EntityFramework_DotNet7_SQLServer/Models/Weapon.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFramework_DotNet7_SQLServer.Models;
+
+public class Weapon
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Damage { get; set; }
+    public Character? Character { get; set; }
+    public int CharacterId { get; set; }
+}
